6b52c3ec37428972a50110fd8b41282426784c03
Josh <joshua.lekay@icloud.com>
2026-06-26 21:44:21 +0200

    Add Nitro plugin for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
+import viteReact from '@vitejs/plugin-react'
 
 export default defineConfig({
-  build: {
-    minify: 'esbuild',
-  },
   plugins: [
-    tailwindcss(),
     tanstackStart(),
+    nitro(),
+    viteReact(),
   ],
 })
